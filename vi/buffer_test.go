fe8753f9d7cd92a84e82ff49b4232292d03b2afd
Laurent Demailly <ldemailly@gmail.com>
2025-07-27 19:05:02 -0700

    A few more tests - not just tab failing

--- vi/buffer_test.go
+++ vi/buffer_test.go
@@ -4,26 +4,38 @@ import (
 	"testing"
 )
 
-func TestInsertWithTabs(t *testing.T) {
+func TestInsertChars(t *testing.T) {
 	v := &Vi{}
 	v.tabs = []int{4, 8, 12, 16, 20} // Set tab stops
 	// v.ap = ansipixels.NewAnsiPixels(0)
 
-	str := "a\tb"
-	// Simulate inserting a string with a tab character one by one
-	var line string
-	for _, r := range str {
-		line = v.buf.InsertChars(v, 0, v.cx, string(r))
-		v.cx++
-		if r == '\t' {
-			v.cx = 4
-		}
-		if line != "" {
-			t.Errorf("Expected empty line after inserting %q, got %q", string(r), line)
-		}
+	tests := []string{
+		"a\tb",
+		"abc",
+		"a🎉",
+		"A乒乓B",
+		"😀🎉",
 	}
-	actual := v.buf.lines[0]
-	if actual != str {
-		t.Errorf("Expected %q got %q", str, actual)
+	for _, str := range tests {
+		v.buf.lines = nil // Reset the buffer lines
+		v.cx = 0          // Reset cursor x position
+		// Simulate inserting a string one by one
+		var line string
+		runes := []rune(str)
+		for i, r := range runes {
+			line = v.buf.InsertChars(v, 0, v.cx, string(r))
+			v.cx++
+			if r == '\t' {
+				v.cx = 4
+			}
+			if line != "" {
+				t.Errorf("Expected empty line after inserting %q, got %q", string(r), line)
+			}
+			actual := v.buf.lines[0]
+			expected := string(runes[:i+1])
+			if actual != expected {
+				t.Errorf("Expected %q got %q", expected, actual)
+			}
+		}
 	}
 }
